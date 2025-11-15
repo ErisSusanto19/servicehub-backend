ea8a954f24cf0285de960cd0f780e303c6213aad
Eris Susanto <erissusanto997@gmail.com>
2025-11-15 20:00:00 +0700

    feat(category): Adds entity component
    - Added `Category` for domain
    - Created `CategoryRequest` and `CategoryResponse` DTOs for data transfer.
    - Added `CategoryRepository` for database interaction.

---
+++ src/main/java/com/eris/servicehub/entities/Category.java
@@ -0,0 +1,27 @@
+package com.eris.servicehub.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    private String description;
+}
